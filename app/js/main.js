d0928f6c140b4df6d1b67c092f3b72d349ef7776
Maria <m1a2r3.kovalenko@gmail.com>
2022-07-09 18:16:06 +0300

    ..

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,35 @@ const swiper = new Swiper(".slider", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+
+
+  slideToClickedSlide: true,
+
+  slidesPerView: 1,
+
+  // spaceBetween: 30,
+
+  // slidesPerColumn: 2,
+
+  direction: "vertical",
+  // direction: "horizontal",
+
+  effect: "fade",
+
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  // breakPoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //   },
+  //   1200: {
+  //     slidesPerView: 3,
+  //   },
+  // },
 });
