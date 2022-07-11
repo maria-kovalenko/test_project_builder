f49464eb3e8215b26f6a5cbb4d49da120689b5bd
Maria <m1a2r3.kovalenko@gmail.com>
2022-07-11 13:24:42 +0300

    переделана структура слайдера, добавлены стили

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,21 @@
-const swiper = new Swiper(".slider", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const sliderMain = document.querySelector(".slider-main");
+const sliderNav = document.querySelector(".slider-nav");
 
-  slideToClickedSlide: true,
+let swiperNav = new Swiper(sliderNav, {
+  slidesPerView: 3,
 
-  slidesPerView: 1,
-  loop: true,
-  loopedSlice: 1,
-
-  // spaceBetween: 30,
-
-  // slidesPerColumn: 2,
-
+  spaceBetween: 30,
+  freeMode: true,
   direction: "vertical",
-  // direction: "horizontal",
+});
 
+let swiperMain = new Swiper(sliderMain, {
+  direction: "vertical",
   effect: "fade",
-
   fadeEffect: {
     crossFade: true,
   },
-
-  // breakPoints: {
-  //   320: {
-  //     slidesPerView: 1,
-  //   },
-  //   768: {
-  //     slidesPerView: 2,
-  //   },
-  //   1200: {
-  //     slidesPerView: 3,
-  //   },
-  // },
-
   thumbs: {
-    swiper: {
-      el: ".slider-mini",
-      slidesPerView: 3,
-      spaceBetween: 30,
-      direction: "vertical",
-      loop: true,
-      loopedSlice: 3,
-    },
+    swiper: swiperNav,
   },
 });
